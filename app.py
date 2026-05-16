aa57b13d61b630d71bc8b1da8afc1ca3d6d8bca7
vytor-official <vytor.official@gmail.com>
2026-05-16 19:44:33 +0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
+import requests
 
+app = Flask(__name__)
+
+NASA_API = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+@app.route("/")
+def home():
+
+    response = requests.get(NASA_API)
+    data = response.json()
+
+    return render_template(
+        "index.html",
+        title=data["title"],
+        image=data["url"],
+        explanation=data["explanation"]
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
