0e39fed1bcf22bdfee2d4a6d775b966878510ffe
vytor-official <vytor.official@gmail.com>
2026-05-16 20:04:22 +0300

    app.py

--- app.py
+++ app.py
@@ -5,18 +5,32 @@ app = Flask(__name__)
 
 NASA_API = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 
+
 @app.route("/")
 def home():
+    try:
+        response = requests.get(NASA_API, timeout=10)
+        data = response.json()
 
-    response = requests.get(NASA_API)
-    data = response.json()
+        if "title" not in data:
+            return f"""
+            <h1>⚠️ NASA API Error</h1>
+            <pre>{data}</pre>
+            """
 
-    return render_template(
-        "index.html",
-        title=data["title"],
-        image=data["url"],
-        explanation=data["explanation"]
-    )
+        return render_template(
+            "index.html",
+            title=data.get("title", "No Title"),
+            image=data.get("url", ""),
+            explanation=data.get("explanation", "No explanation available")
+        )
+
+    except Exception as e:
+        return f"""
+        <h1>⚠️ Server Error</h1>
+        <pre>{str(e)}</pre>
+        """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
