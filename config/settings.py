edd1c379aa96c1d93e4ebd823b4a4ac51476c047
vytor-official <vytor.official@gmail.com>
2026-05-17 17:01:13 +0300

    Add centralized settings module

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,9 @@
-NASA_API_KEY = "YOUR_NASA_API_KEY"
+import os
 
+APP_NAME = "Vytor Space Intelligence"
+NASA_API_KEY = os.getenv("NASA_API_KEY", "")
 NASA_APOD_URL = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
 NASA_ASTEROID_URL = f"https://api.nasa.gov/neo/rest/v1/feed?api_key={NASA_API_KEY}"
 ISS_POSITION_URL = "http://api.open-notify.org/iss-now.json"
-REQUEST_TIMEOUT = 10 
+ISS_STREAM_URL = "https://www.youtube.com/embed/OCem0E-0Q6Y"
+REQUEST_TIMEOUT = 10
