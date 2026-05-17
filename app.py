570c020e26bb9776e53dab2938500fce4c2ef819
vytor-official <vytor.official@gmail.com>
2026-05-17 15:11:24 +0300

    Create Flask application orchestrator
    Create the web entrypoint and connect the core engine to the template layer.

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
- 
+from flask import Flask, render_template
+from core.engine import get_dashboard_data
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data = get_dashboard_data()
+    return render_template("index.html", **data)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
